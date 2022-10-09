cfdec4a5c2aa7eb147bc3f7607c8c2030b56f044
Josh Farr <josh.farr@wsu.edu>
2022-10-09 12:58:53 -0700

    This is a test to see if updates are pushed to the s3 bucket

--- front-end/index-vue/src/components/HelloWorld.vue
+++ front-end/index-vue/src/components/HelloWorld.vue
@@ -2,8 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      This is a test to see if updates are pushed to the s3 bucket
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
