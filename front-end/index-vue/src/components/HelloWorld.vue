f6205e0fa8ad41fd301f2fa10d4026c7eae50d78
Josh Farr <josh.farr@wsu.edu>
2022-10-09 14:12:45 -0700

    added basic events.json and displayed it on homepage for testing

--- front-end/index-vue/src/components/HelloWorld.vue
+++ front-end/index-vue/src/components/HelloWorld.vue
@@ -1,41 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      This is a test to see if updates are pushed to the s3 bucket
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+<div>
+    <h1>display events</h1>
+
+    <div v-for="Event in eventDataList" :key="Event.id">
+      <div>
+        <div>
+          <span>{{Event.title}}  {{Event.description}} {{Event.link}} {{Event.time}} {{Event.image}}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+//"@/assets/events.json"
+import axios from "axios";
+
+    export default {
+        name: "eventList",
+        data() {
+            return {
+                eventDataList: []
+            };
+        },
+        methods: {
+            getEventData() {
+                axios
+                    .get("events.json")
+                    .then(response => (this.eventDataList = response.data));
+            }
+        },
+        beforeMount()
+        {
+        this.getEventData();
+        }
+    };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -55,3 +55,6 @@ a {
   color: #42b983;
 }
 </style>
+
+<!-- <button v-on:click="getEventData">Get events Data</button>
+<div>{{eventDataList}}</div> -->
