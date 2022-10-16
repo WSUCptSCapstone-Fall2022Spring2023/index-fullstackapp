b8406d44bd28ffb2e118335180d427a736ac1387
Josh Farr <josh.farr@wsu.edu>
2022-10-15 18:38:39 -0700

    removing unessesary files

--- index-editor-app/index-editor-app-engine/IndexAPIClient.cs
+++ index-editor-app/index-editor-app-engine/IndexAPIClient.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace index_editor_app_engine
 {
-    internal class IndexAPIClient
+    public class IndexAPIClient
     {
-    }
+        
+        private HttpClient httpClient;
+        private object _config;
+
+        public IndexAPIClient()
+        {
+            //create http client
+            httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromMinutes(3);
+
+        }
+
+        public async Task<bool> TestConnection()
+        {
+            try
+            {
+                var builder = new UriBuilder($"{_config.OPEN_SEARCH_ENDPOINT}");
+                var httpResponse = await httpClient.GetAsync(builder.ToString());
+
+                return httpResponse.IsSuccessStatusCode;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        }
+
+
 }
