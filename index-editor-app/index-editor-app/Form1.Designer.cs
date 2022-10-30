75344d86163bb22737864d0f39aa1a9557a1532a
Josh Farr <josh.farr@wsu.edu>
2022-10-30 13:53:52 -0700

    added datagrid to display all events with an edit button. Edit button fills in textboxes

--- index-editor-app/index-editor-app/Form1.Designer.cs
+++ index-editor-app/index-editor-app/Form1.Designer.cs
@@ -28,59 +28,353 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.buttonSend = new System.Windows.Forms.Button();
-            this.buttonGET = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CreateEvent = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LinktextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.descriptionBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tabControl1
             // 
-            this.textBox1.Location = new System.Drawing.Point(74, 45);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(658, 383);
-            this.textBox1.TabIndex = 2;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 27);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1472, 521);
+            this.tabControl1.TabIndex = 6;
             // 
-            // buttonSend
+            // tabPage1
             // 
-            this.buttonSend.Location = new System.Drawing.Point(713, 415);
-            this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(75, 23);
-            this.buttonSend.TabIndex = 4;
-            this.buttonSend.Text = "SEND";
-            this.buttonSend.UseVisualStyleBackColor = true;
-            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.CreateEvent);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.dateTimePicker1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.LinktextBox);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.DeleteButton);
+            this.tabPage1.Controls.Add(this.descriptionBox1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1464, 493);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Events";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // buttonGET
+            // button1
             // 
-            this.buttonGET.Location = new System.Drawing.Point(632, 415);
-            this.buttonGET.Name = "buttonGET";
-            this.buttonGET.Size = new System.Drawing.Size(75, 23);
-            this.buttonGET.TabIndex = 5;
-            this.buttonGET.Text = "GET";
-            this.buttonGET.UseVisualStyleBackColor = true;
-            this.buttonGET.Click += new System.EventHandler(this.buttonGET_Click);
+            this.button1.Location = new System.Drawing.Point(1295, 464);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Confirm Changes";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(601, 172);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(142, 15);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "This event was created on";
+            // 
+            // CreateEvent
+            // 
+            this.CreateEvent.Location = new System.Drawing.Point(7, 168);
+            this.CreateEvent.Name = "CreateEvent";
+            this.CreateEvent.Size = new System.Drawing.Size(118, 23);
+            this.CreateEvent.TabIndex = 16;
+            this.CreateEvent.Text = "Create new event";
+            this.CreateEvent.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1041, 215);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 155);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1111, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Image";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(794, 380);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(817, 362);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "End time";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(482, 380);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(564, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(704, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Start time";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(688, 380);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(487, 432);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Link";
+            // 
+            // LinktextBox
+            // 
+            this.LinktextBox.Location = new System.Drawing.Point(487, 450);
+            this.LinktextBox.Multiline = true;
+            this.LinktextBox.Name = "LinktextBox";
+            this.LinktextBox.Size = new System.Drawing.Size(447, 24);
+            this.LinktextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Description";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1350, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(1350, 193);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 2;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // descriptionBox1
+            // 
+            this.descriptionBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.descriptionBox1.Location = new System.Drawing.Point(6, 271);
+            this.descriptionBox1.Multiline = true;
+            this.descriptionBox1.Name = "descriptionBox1";
+            this.descriptionBox1.Size = new System.Drawing.Size(460, 216);
+            this.descriptionBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1452, 157);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1464, 493);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Meetings";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1464, 493);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "News";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1496, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.buttonGET);
-            this.Controls.Add(this.buttonSend);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1496, 560);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_LoadAsync);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private TextBox textBox1;
-        private Button buttonSend;
-        private Button buttonGET;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private DataGridView dataGridView1;
+        private TabPage tabPage3;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuToolStripMenuItem;
+        private ToolStripMenuItem refreshToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem loadToolStripMenuItem;
+        private TextBox descriptionBox1;
+        private Button button2;
+        private Button DeleteButton;
+        private Button CreateEvent;
+        private PictureBox pictureBox1;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label5;
+        private DateTimePicker dateTimePicker1;
+        private Label label4;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label2;
+        private TextBox LinktextBox;
+        private Label label1;
+        private Label label7;
+        private Button button1;
+        private OpenFileDialog openFileDialog1;
     }
 }
--- index-editor-app/index-editor-app/Form1.cs
+++ index-editor-app/index-editor-app/Form1.cs
@@ -8,66 +8,181 @@ namespace index_editor_app
 {
     public partial class Form1 : Form
     {
+        IndexAPIClient indexClient;
+
+        Events[] events;
+        //News[] news;
+        //Meetings[] meetings;
+
+        string eventsJson;
+        string newsJson;
+        string meetingsJson;
+
+        int eventsCount = 0;
+        int newsCount = 0;
+        int meetingsCount = 0;
+
+        int edtingEventIndex;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_LoadAsync(object sender, EventArgs e)
+        private async void Form1_LoadAsync(object sender, EventArgs e)
         {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.ImageLocation = "C:/Users/josh/Desktop/testimage.png";
 
+            //create instance of client
+            this.indexClient = new IndexAPIClient();
 
+            //test connection to API and get data
+            if (await TestConnection())
+            {
+                this.eventsJson = await indexClient.GetDocument();
+                this.events = JsonConvert.DeserializeObject<Events[]>(eventsJson);
+                this.eventsCount = events.Count();
+                InitializeDataGrid();
+            }
         }
-
-        private async void buttonSend_Click(object sender, EventArgs e)
+        public async Task<bool> TestConnection()
         {
-            //create index client
-            IndexAPIClient indexClient = new IndexAPIClient();
-            //test connection
             bool test = await indexClient.TestConnection();
-            string message = "";
             if (test)
             {
-                message = "Successfuly connected to index";
+                return true;
+            }
+            System.Windows.Forms.MessageBox.Show("Connection fialed!" + "\n" + "See \"some ducument.txt\" for help");
+            return false;
+        }
+
+        public void InitializeDataGrid()
+        {
+            //clear the datagrid
+            this.dataGridView1.CancelEdit();
+            this.dataGridView1.Columns.Clear();
+            this.dataGridView1.Rows.Clear();
+            this.dataGridView1.DataSource = null;
+
+            //add event headers
+            this.dataGridView1.Columns.Add("title", "Title");
+            this.dataGridView1.Columns.Add("time", "Creation Date");
+            DataGridViewColumn TitleColumn = dataGridView1.Columns[0];
+            DataGridViewColumn DateColumn = dataGridView1.Columns[1];
+            DateColumn.Width = 70;
+            TitleColumn.Width = 565;
+
+
+            //add row index
+            this.dataGridView1.Rows.Add(events.Count());
+            foreach (DataGridViewRow row in this.dataGridView1.Rows)
+            {
+                row.HeaderCell.Value = string.Format("{0}", row.Index + 1);
+            }
+
+            //add event data
+            for (int i = 0; i < eventsCount; i++)
+            {
+                dataGridView1[0, i].Value = events[i].Title;
+                //dataGridView1[1, i].Value = events[i].Time; 
+                dataGridView1[1, i].Value = DateTime.Now.ToString("MM/dd/yyyy");
+            }
+
+            //add edit button
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            dataGridView1.Columns.Add(btn);
+            btn.Width = 60;
+            btn.HeaderText = "Edit";//column header
+            btn.Text = "Edit";
+            btn.Name = "Edit";
+            btn.UseColumnTextForButtonValue = true;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            descriptionBox1.Text = e.ColumnIndex.ToString();
+
+            if (e.ColumnIndex == 2)
+            {
+                pictureBox1.ImageLocation = "/Images/testimage.png";
+                //events.json, need to add creationDate, start and end time
+                this.edtingEventIndex = e.RowIndex;
+                dateTimePicker1.Text = DateTime.Now.ToString();
+
+                descriptionBox1.Text = events[edtingEventIndex].Description;
+                LinktextBox.Text = events[edtingEventIndex].Link;
+
+            }
+        }
+
+
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Are you sure to delete this item ??", "Confirm Delete!!", MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                descriptionBox1.Text = "Deleted" + events[edtingEventIndex].Title;
             }
             else
             {
-                textBox1.Text = "failed to connect to index";
-                return;
+                //textBox1.Text = "Deleted";
             }
 
-            //read the updated string
-            string updatedjson = textBox1.Text;
-            string output = "[" + updatedjson.Split('[', ']')[1] + "]";
-            textBox1.Text = output;
-            indexClient.PutDocument(output);
 
         }
 
-        private async void buttonGET_Click(object sender, EventArgs e)
-        {
-            //create index client
-            IndexAPIClient indexClient = new IndexAPIClient();
-            //test connection
-            bool test = await indexClient.TestConnection();
-            string message = "";
-            if (test)
-            {
-                message = "Successfuly connected to index";
-            }
-            else
-            {
-                textBox1.Text = "failed to connect to index";
-                return;
-            }
 
-            //GetEvents
-            string eventsJson = await indexClient.GetDocument();
 
-            //store json events data as a class
-            Events[] events = JsonConvert.DeserializeObject<Events[]>(eventsJson);
+        //private async void buttonSend_Click(object sender, EventArgs e)
+        //{
+        //    //create index client
+        //    IndexAPIClient indexClient = new IndexAPIClient();
+        //    //test connection
+        //    bool test = await indexClient.TestConnection();
+        //    string message = "";
+        //    if (test)
+        //    {
+        //        message = "Successfuly connected to index";
+        //    }
+        //    else
+        //    {
+        //        textBox1.Text = "failed to connect to index";
+        //        return;
+        //    }
 
-            textBox1.Text = message + Environment.NewLine + "Current events" + Environment.NewLine + eventsJson;
-        }
+        //    //read the updated string
+        //    string updatedjson = textBox1.Text;
+        //    string output = "[" + updatedjson.Split('[', ']')[1] + "]";
+        //    textBox1.Text = output;
+        //    indexClient.PutDocument(output);
+
+        //}
+
+        //private async void buttonGET_Click(object sender, EventArgs e)
+        //{
+        //    //create index client
+        //    IndexAPIClient indexClient = new IndexAPIClient();
+        //    //test connection
+        //    bool test = await indexClient.TestConnection();
+        //    string message = "";
+        //    if (test)
+        //    {
+        //        message = "Successfuly connected to index";
+        //    }
+        //    else
+        //    {
+        //        textBox1.Text = "failed to connect to index";
+        //        return;
+        //    }
+
+        //    //GetEvents
+        //    string eventsJson = await indexClient.GetDocument();
+
+        //    //store json events data as a class
+        //    Events[] events = JsonConvert.DeserializeObject<Events[]>(eventsJson);
+
+        //    textBox1.Text = message + Environment.NewLine + "Current events" + Environment.NewLine + eventsJson;
+        //}
     }
 }
