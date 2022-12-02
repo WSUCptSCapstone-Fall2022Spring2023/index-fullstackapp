a1923bd5c2d309ec4b6896b37de3c02b4142f8ed
tdphuochuy <tdphuochuy@gmail.com>
2022-12-02 09:11:55 -0800

    Added Vue Router

--- front-end/index-vue/src/App.vue
+++ front-end/index-vue/src/App.vue
@@ -1,16 +1,79 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <link
+      rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous"
+    />
+  </header>
+  <nav id = "navbar" class="navbar">
+      <div class="navbar__container">
+        <div class ="logo-box">
+          <img src="./assets/logo.png" style ="width: 60px;height: 60px;">
+          <div class = "site-title-block">
+            <h2>INDEx</h2>
+            <p>Inland Northwest Disability Experience</p>
+          </div>
+      </div>
+        <div class="navbar__toggle" id="mobile-menu">
+          <span class="bar"></span> 
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+        <ul class="navbar__menu">
+          <li class="navbar__item">
+            <a class="navbar__links">Home</a>
+          </li>
+          <li class="navbar__item">
+            <a class="navbar__links">About us</a>
+          </li>
+          <li class="navbar__item">
+            <a class="navbar__links">Get Involved</a>
+          </li>
+          <li class="navbar__item">
+            <a class="navbar__links">Contact</a>
+          </li>
+          <li class="navbar__item">
+            <a class="navbar__links">Resources</a>
+          </li>
+          <li class="navbar__item">
+            <a class="navbar__links">Services</a>
+          </li>
+          <li class="navbar__item">
+            <a class="navbar__links">Events</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  window.onload = function(){
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    // Display Mobile Menu
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.navbar__menu');
+    const mobileMenu = () => {
+      menu.classList.toggle('is-active');
+      menuLinks.classList.toggle('active');
+    };
+    menu.addEventListener('click', mobileMenu);
+
+    //Nav bar scroll listener
+    window.addEventListener('scroll', highlightMenu);
+  };
+
+  const highlightMenu = () => {
+      let navbar = document.getElementById('navbar');
+      let scrollPos = window.scrollY;
+      if (scrollPos < 100)
+      {
+        navbar.classList.add('navbar');
+        navbar.classList.remove('navbarscrolled');
+      } else {
+        navbar.classList.remove('navbar');
+        navbar.classList.add('navbarscrolled');
+      }
+    };
 </script>
 
 <style>
@@ -20,5 +83,166 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: black;
+}
+
+/* Nav Bar Section */
+.logo-box{
+  display: flex;
+}
+
+.site-title-block{
+  margin-left: 10px;
+  color: #306eb9;
+  text-align: left;
+  line-height: 1px;
+}
+
+.navbar {
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+  transition: 0.2s linear all;
+  padding: 10px;
+  margin: 0 auto;
+  max-width: 1200px;
+}
+
+.navbarscrolled {
+    box-shadow: inset 0 0 0 200px rgb(255 255 255 / 5%);
+    backdrop-filter: blur(10px);
+    background: rgba(255, 255, 255,0.8);
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+    position: sticky;
+    top: 0;
+    z-index: 999;
+    transition: 0.5s linear all;
+    padding: 10px;
+    margin: 0 auto;
+    max-width: 1200px;
+}
+
+.navbar__links {
+  color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  height: 100%;
+  transition: all 0.3s ease;
+  cursor: pointer;
+}
+
+.navbar__container {
+  display: flex;
+  justify-content: space-between;
+  z-index: 1;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.navbar__menu {
+  display: flex;
+  align-items: center;
+  list-style: none;
+}
+
+.navbar__item {
+  padding: 10px;
+}
+
+.navbar__toggle{
+  cursor: pointer;
+}
+
+.navbar__toggle .bar {
+    display: block;
+}
+
+@media screen and (max-width: 715px) {
+  .navbar__container {
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    z-index: 1;
+    width: 100%;
+    max-width: 100%;
+    padding: 0;
+  }
+
+  .navbar__menu {
+    display: grid;
+    grid-template-columns: auto;
+    margin: 0;
+    width: 100%;
+    position: absolute;
+    top: -1000px;
+    opacity: 1;
+    transition: all 0.5s ease;
+    z-index: -1;
+    right: 0;
+  }
+
+  .navbar__menu.active {
+    top: 100%;
+    opacity: 1;
+    transition: all 0.5s ease;
+    z-index: 99;
+    font-size: 1.6rem;
+    background: #ffffff;
+  }
+
+  .navbar__toggle .bar {
+    width: 25px;
+    height: 3px;
+    margin: 5px auto;
+    transition: all 0.3s ease-in-out;
+    background: rgb(0, 0, 0);
+  }
+
+  .navbar__item {
+    position: relative;
+    width: 100%;
+    padding: 0;
+  }
+
+  .navbar__links {
+    text-align: center;
+    padding: 2rem;
+    left: 0;
+    right: 0;
+  }
+
+  #mobile-menu {
+    position: absolute;
+    top: 20%;
+    right: 5%;
+    transform: translate(5%, 20%);
+  }
+
+  .navbar__toggle .bar {
+    display: block;
+    cursor: pointer;
+  }
+
+  #mobile-menu.is-active .bar:nth-child(2) {
+    opacity: 0;
+  }
+
+  #mobile-menu.is-active .bar:nth-child(1) {
+    transform: translateY(8px) rotate(45deg);
+  }
+
+  #mobile-menu.is-active .bar:nth-child(3) {
+    transform: translateY(-8px) rotate(-45deg);
+  }
 }
 </style>
---
+++ front-end/index-vue/src/components/EventsPage.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class = "row">
+        <div v-for="event in events" :key="event.title" class="events-container">
+            <article class="post">
+                <img class="event-img" v-bind:src='event.image'>
+                <div class="description-box">
+                    <h2>{{event.title}}</h2>
+                    <div><i class="fa fa-calendar-day"></i><b>{{event.start_date}}</b></div>
+                    <div><i class="fa fa-clock"></i><b>{{event.time_range}}</b></div>
+                    <p>{{event.description}}</p>
+                </div>
+            </article>
+        </div>
+    </div>
+</template>
+
+<script>
+    let json = require('../../public/events.json');
+    export default {
+        data(){
+            return {
+                events : json
+            }
+        }
+    }
+    
+</script>
+
+<style>
+    .row {
+        margin: 0 auto;
+        max-width: 1200px;
+    }
+
+    .events-container{
+        max-width: 800px;
+    }
+
+    .post{
+        display: flex;
+        border: 1px solid gray;
+        padding: 20px;
+        grid-column-gap: 20px;
+        margin-bottom: 20px;
+    }
+    .event-img{
+        width: auto;
+        max-height: 225px;
+        max-width: 338px;
+        height: auto;
+    }
+
+    .description-box{
+    text-align: left;
+    }
+
+    
+</style>
